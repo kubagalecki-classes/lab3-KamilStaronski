f6f0261ee1d5090d956fec6b38b0a913fba27c91
KamilStaronski <73498649+KamilStaronski@users.noreply.github.com>
2020-11-18 18:51:16 +0100

    Update transportujFlota.hpp

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -4,6 +4,16 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    Stocznia stocznia;
+    int suma=0;
+    while(suma<towar){
+        Statek* nowy_statek=stocznia();
+        int wartosc = nowy_statek->transportuj();
+        suma+=wartosc;
+        //rzutowanie jeśli rózne od 0
+        
+        delete nowy_statek;
+    }
+    std::cout<<endl<<"suma = "<<suma<<endl;
+    return suma;
 }
