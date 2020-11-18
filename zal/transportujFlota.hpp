49291693a4369de37bf208d988cff9119afa0cdc
KamilStaronski <73498649+KamilStaronski@users.noreply.github.com>
2020-11-18 20:16:05 +0100

    Update transportujFlota.hpp

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -6,14 +6,24 @@ unsigned int transportujFlota(unsigned int towar)
 {
     Stocznia stocznia;
     int suma=0;
+    int licznik=0;
     while(suma<towar){
-        Statek* nowy_statek=stocznia();
-        int wartosc = nowy_statek->transportuj();
-        suma+=wartosc;
-        //rzutowanie jeśli rózne od 0
-        
+        Statek* nowy_statek = stocznia();
+        int wartosc = nowy_statek -> transportuj();
+        suma += wartosc;
+        Zaglowiec* rzutowany = dynamic_cast <Zaglowiec*> (nowy_statek);
+        if(rzutowany!=0) 
+        {
+            licznik++;
+        }
+        std::cout<<rzutowany<<endl;
         delete nowy_statek;
     }
-    std::cout<<endl<<"suma = "<<suma<<endl;
-    return suma;
+    std::cout<<std::endl<<"suma = "<<suma<<std::endl;
+    //return suma;
+    
+    
+    std::cout<<endl<<"zaglowce = "<<licznik<<endl;
+    return licznik;
 }
+
